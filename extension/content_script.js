367833f6f86c73631c8a0da9c91babbb7a5b04fd
Raymond Willett <rwillet2@emich.edu>
2017-07-12 00:32:35 -0400

    annyang speech recognition

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,38 +1,123 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("Received request: ", request);
-    let eventName = request.eventName;
-    if (events[eventName]) {
-      document.dispatchEvent(events[eventName]);
-      sendResponse(createResponseSuccessMessage());
+function couchSurf() {
+  function seekHandler(seekTime) {
+    netflix.cadmium.UiEvents.events.resize[1].scope.events.dragend[1].handler(null, {value: seekTime, pointerEventData: {}});
+  }
+
+  function convertToSeconds(minutes, seconds) {
+    let time = Number(seconds);
+    time += (Number(minutes) * 60);
+    if (!isNaN(time)) {
+      return time;
     } else {
-      alert("That action is not supported");
-      sendResponse(createResponseFailureMessage());
+      throw error(createLoggingObject("The calculated time was not a number."));
     }
-  });
+  }
 
+  function calculateOffsetSeekTime(seconds) {
+    let time = document.querySelector('video').currentTime;
+    time += convertToSeconds(0, seconds);
+    return time;
+  }
 
-document.addEventListener('play', function (e) {
-    console.log('pressed the play button');
-  }, false);
-document.addEventListener('pause', function (e) {
-    console.log('pressed the pause button');
-  }, false);
+  function createLoggingObject(message) {
+    return {
+        "error": message,
+        "arguments": arguments,
+        "date": Date()
+      };
+  }
 
-var events = {
-    "play": new Event('netflix_video_play'),
-    "pause": new Event('netflix_video_pause'),
-    "seek": new Event('netflix_video_seek')
+  function initCommands() {
+    let commands = {
+      'pause netflix': function () {
+        document.querySelector('video').pause();
+      },
+      'play netflix': function () {
+        document.querySelector('video').play();
+      },
+      'go to :minutes (minutes) and :seconds (seconds)': function (minutes, seconds) {
+        if (isNaN(minutes) || isNaN(seconds)) {
+          return;
+        } else {
+          const seekTime = convertToSeconds(minutes, seconds);
+          seekHandler(seekTime);
+        }
+      },
+      'go back :seconds (seconds)': function(seconds) {
+        if (isNaN(seconds)) {
+          return;
+        } else {
+          const seekTime = calculateOffsetSeekTime(-seconds);
+          seekHandler(seekTime);
+        }
+      },
+      'go forward :seconds (seconds)': function(seconds) {
+        if (isNaN(seconds)) {
+          return;
+        } else {
+          const seekTime = calculateOffsetSeekTime(seconds);
+          seekHandler(seekTime);
+        }
+      }
+    }
+    annyang.addCommands(commands);
+  }
+  
+  function importAnnyang() {
+    let imported_script = document.createElement('script');
+    imported_script.src = "https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js";
+    document.head.appendChild(imported_script);
+    return;
+  }
+
+  function initCallbacks() {
+    annyang.addCallback('result', function(results) {
+      console.log(results);
+    });
+  } 
+
+  function init() {
+    console.log('initializing');
+    //importAnnyang();
+    if (typeof annyang === "undefined") {
+      window.setTimeout(function () {
+        initCommands();
+        initCallbacks();
+        annyang.start();
+      }, 1000);
+    } else {
+      initCommands();
+      initCallbacks();
+      annyang.start();
+    }
+    console.log('Annyang is listening!');
+  }
+  init();
 }
+couchSurf();
 
-function createResponseSuccessMessage() {
-  return {
-    success: true
-  };
-}
 
-function createResponseFailureMessage() {
-  return {
-    success: false
-  };
-}
+// content_script has access to DOM, but not the same window object. Gonna have to get clever, here.
+
+
+var triggerEvent = function triggerEvent () {
+    var event = document.createEvent('Event');
+    event.initEvent('scriptInjected', true, true);
+    document.dispatchEvent(event);
+ };
+ var addEventListener = function addListenerEvent () { 
+   document.addEventListener('scriptInjected', function(e) {
+     let seekTime = 100;
+     alert(netflix);
+     netflix.cadmium.UiEvents.events.resize[1].scope.events.dragend[1].handler(null, {value: seekTime, pointerEventData: {}});
+    }
+  )};
+
+ var trigger = document.createElement('script');
+ trigger.innerText = triggerEvent.toString() + "\n triggerEvent();";
+
+ var listener = document.createElement('script');
+ listener.innerText = addEventListener.toString() + "\n addListenerEvent();";
+
+ document.head.appendChild(listener);
+ document.head.appendChild(trigger);
--- extension/popup.js
+++ extension/popup.js
@@ -1,22 +1,3 @@
-const eventButtons = [
-  {
-    eventName: "netflix_video_play",
-    buttonID: "play"
-  },
-  {
-    eventName: "netflix_video_pause",
-    buttonID: "pause"
-  }
-];
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  for(let event of eventButtons) {
-    document.getElementById(event.buttonID).addEventListener('click', () => { sendMessageFor(tabs, event.eventName) });
-  }
-});
-
-function sendMessageFor(tabs, eventName) {
-  chrome.tabs.sendMessage(tabs[0].id, {eventName: eventName}, function(response) {
-    console.log(response.success);
-  });
+function doNothing() {
+  return;
 }
